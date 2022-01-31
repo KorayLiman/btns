1d0f8529dbdbaa766d199da8b9058c7749b04430
Koray Liman <69529444+KorayLiman@users.noreply.github.com>
2022-01-31 09:40:49 +0300

    sadadg

---
+++ lib/buttons.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class BasicButtons extends StatelessWidget {
+  const BasicButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+              onPressed: () {},
+              onLongPress: () {
+                debugPrint("long press");
+              },
+              child: const Text("TextButton")),
+          TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.access_alarm),
+              style: ButtonStyle(
+                  //backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  backgroundColor: MaterialStateProperty.resolveWith((states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.green;
+                    }
+                    return null;
+                  }),
+                  overlayColor: MaterialStateProperty.all(
+                      Colors.yellow.withOpacity(0.6))),
+              label: const Text("TextIconButton")),
+          ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.red, onPrimary: Colors.green),
+              child: const Text("Elevated Button")),
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.accessibility_new),
+              label: const Text("Elevated Button with icon")),
+          OutlinedButton(onPressed: () {}, child: const Text("Outlined")),
+          OutlinedButton.icon(
+              style: OutlinedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                  side: const BorderSide(color: Colors.green, width: 4)),
+              onPressed: () {},
+              icon: const Icon(Icons.account_balance_wallet_sharp),
+              label: const Text("outlined with icon")),
+          OutlinedButton.icon(
+              style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  side: const BorderSide(color: Colors.green, width: 4)),
+              onPressed: () {},
+              icon: const Icon(Icons.account_balance_wallet_sharp),
+              label: const Text("outlined with icon"))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_4/dropdownbutton.dart';
+
+import 'buttons.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "MyApp",
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            textTheme: TextTheme(
+                headline1: TextStyle(
+                    color: Colors.purple, fontWeight: FontWeight.bold)),
+            outlinedButtonTheme:
+                OutlinedButtonThemeData(style: OutlinedButton.styleFrom()),
+            textButtonTheme: TextButtonThemeData(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue)))),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Buttons"),
+          ),
+          body: DropDownButtonUsage(),
+        ));
+  }
+}
